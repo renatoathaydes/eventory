aa044bfa3877dcefaf12bd86cd32dcf2df9673c4
renatoathaydes <renatoathaydes@hotmail.com>
2019-08-11 10:49:29 +0200

    Added option to test batch additions in eventory example. Found batch additions are much faster. Optimal size around 1024 events.

--- example/eventory_example.dart
+++ example/eventory_example.dart
@@ -5,8 +5,9 @@ import 'package:eventory/src/file_event_sink.dart';
 
 void main(List<String> args) async {
   int eventsPerCycle = args.isNotEmpty ? int.parse(args[0]) : 100000;
-
-  print("Creating $eventsPerCycle events");
+  int batchSize = args.length > 1 ? int.parse(args[1]) : 0;
+  print(
+      "Creating $eventsPerCycle events${batchSize > 0 ? ' using batches of $batchSize events' : ''}");
 
   var events = <Event>[];
   for (var i = 0; i < eventsPerCycle; i++) {
@@ -21,15 +22,16 @@ void main(List<String> args) async {
   print("Starting to write events to file ${tempFile.path}.");
 
   watch.start();
-  for (var event in events) {
-    await sink.add(event);
+  if (batchSize > 0) {
+    await sendEventsInBatches(events, sink, batchSize);
+  } else {
+    await sendEventsOneByOne(events, sink);
   }
   print(
       "Done sending events in ${watch.elapsedMilliseconds} ms. Flushing events.");
 
   // will close and wait for file handle to get flushed
   await sink.close();
-
   watch.stop();
 
   events = null;
@@ -37,7 +39,8 @@ void main(List<String> args) async {
 
   print("All events written in ${watch.elapsedMilliseconds} ms total.");
   print("Write throughput: "
-      "${(await tempFile.length()).toDouble() * 1e-6 / watch.elapsed.inSeconds}"
+      "${(await tempFile.length()).toDouble() * 1.0e-6 /
+      (watch.elapsed.inMilliseconds * 1e-3)}"
       " MB/sec");
 
   print("Checking for correctness...");
@@ -50,4 +53,23 @@ void main(List<String> args) async {
     }
   }
   print("OK!");
+}
+
+void sendEventsOneByOne(List<Event> events, Sink<Event> sink) async {
+  for (var event in events) {
+    await sink.add(event);
+  }
+}
+
+void sendEventsInBatches(
+    List<Event> events, EventorySink sink, int batchSize) async {
+  final batch = <Event>[];
+  for (var i = 0; i < events.length; i++) {
+    batch.add(events[i]);
+    if (i != 0 && i % batchSize == 0) {
+      await sink.addBatch(batch);
+      batch.clear();
+    }
+  }
+  await sink.addBatch(batch);
 }
