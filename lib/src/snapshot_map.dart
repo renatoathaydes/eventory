c942ae46be45380c558f78e69606d84559a57a2a
renatoathaydes <renatoathaydes@hotmail.com>
2019-08-15 22:03:01 +0200

    Fixed SnapshotMap. Needs to be able to merge values.

--- lib/src/snapshot_map.dart
+++ lib/src/snapshot_map.dart
@@ -1,23 +1,31 @@
 import 'dart:collection';
 
 import 'package:collection/collection.dart';
-import 'package:eventory/src/util.dart';
 
-// TODO remove this once https://github.com/dart-lang/collection/pull/110 is merged
-class SnapshotMap<K, V> extends CombinedMapView<K, V> {
-  Iterable<K> Function() _lazyKeys;
+class SnapshotMap<K, V> extends UnmodifiableMapBase<K, V> {
+  final Map<K, V> topMap;
+  final Map<K, V> bottomMap;
+  final V Function(V top, V bottom) combine;
 
-  SnapshotMap(Map<K, V> topMap, Map<K, V> bottomMap)
-      : super([topMap, bottomMap]) {
-    values;
-    _lazyKeys = lazy(() => super.keys.toSet());
-  }
+  SnapshotMap(this.topMap, this.bottomMap, this.combine);
 
   UnmodifiableMapView<K, V> get view => UnmodifiableMapView(this);
 
   @override
-  Iterable<K> get keys => _lazyKeys();
+  Iterable<K> get keys {
+    return topMap.keys
+        .followedBy(bottomMap.keys.where((k) => !topMap.containsKey(k)));
+  }
 
   @override
   int get length => keys.length;
+
+  @override
+  V operator [](Object key) {
+    final top = topMap[key];
+    final bottom = bottomMap[key];
+    if (top == null) return bottom;
+    if (bottom == null) return top;
+    return combine(top, bottom);
+  }
 }
--- test/snapshot_map_test.dart
+++ test/snapshot_map_test.dart
@@ -1,15 +1,17 @@
 import 'package:eventory/src/snapshot_map.dart';
 import 'package:test/test.dart';
 
+int combiner(int a, int b) => a + b;
+
 void main() {
   test('SnapshotMap can be empty', () {
-    final map = SnapshotMap({}, {});
+    final map = SnapshotMap(<int, int>{}, <int, int>{}, combiner);
     expect(map.isEmpty, isTrue);
     expect(map.length, equals(0));
   });
 
   test('SnapshotMap can contain elements in only bottom map', () {
-    final map = SnapshotMap({}, {'1': 1, '2': 2});
+    final map = SnapshotMap<String, int>({}, {'1': 1, '2': 2}, combiner);
     expect(map.isEmpty, isFalse);
     expect(map.length, equals(2));
     expect(map.keys.toList(), equals(['1', '2']));
@@ -23,7 +25,7 @@ void main() {
   });
 
   test('SnapshotMap can contain elements in only top map', () {
-    final map = SnapshotMap({'1': 1, '2': 2}, {});
+    final map = SnapshotMap<String, int>({'1': 1, '2': 2}, {}, combiner);
     expect(map.isEmpty, isFalse);
     expect(map.length, equals(2));
     expect(map.keys.toList(), equals(['1', '2']));
@@ -37,7 +39,7 @@ void main() {
   });
 
   test('SnapshotMap can contain elements in both maps', () {
-    final map = SnapshotMap({'1': 1}, {'2': 2});
+    final map = SnapshotMap<String, int>({'1': 1}, {'2': 2}, combiner);
     expect(map.isEmpty, isFalse);
     expect(map.length, equals(2));
     expect(map.keys.toList(), equals(['1', '2']));
@@ -50,31 +52,33 @@ void main() {
     expect(map['3'], isNull);
   });
 
-  test('SnapshotMap - top Map overrides bottom Map elements', () {
-    final map = SnapshotMap({'1': 1, '2': 2, '3': 3}, {'2': 22, '4': 44});
+  test('SnapshotMap - top Map combines with bottom Map elements', () {
+    final map = SnapshotMap<String, int>(
+        {'1': 1, '2': 2, '3': 3}, {'2': 22, '4': 44}, combiner);
     expect(map.isEmpty, isFalse);
     expect(map.length, equals(4));
     expect(map.keys.toList(), equals(['1', '2', '3', '4']));
-    expect(map.values.toList(), equals([1, 2, 3, 44]));
+    expect(map.values.toList(), equals([1, 24, 3, 44]));
     expect(map['1'], equals(1));
-    expect(map['2'], equals(2));
+    expect(map['2'], equals(24));
     expect(map['3'], equals(3));
     expect(map['4'], equals(44));
   });
 
   test('SnapshotMap - members can also be SnapshotMap', () {
-    final topMap = SnapshotMap({'1': 1, '2': 2}, {'2': 22, '4': 44});
-    final bottomMap = SnapshotMap({'1': 11, '3': 3}, // keep line
-        {'1': 111, '4': 4, '5': 5});
-    final map = SnapshotMap(topMap, bottomMap);
+    final topMap = SnapshotMap<String, int>(
+        {'1': 1, '2': 2}, {'2': 22, '4': 44}, combiner);
+    final bottomMap = SnapshotMap<String, int>({'1': 11, '3': 3}, // keep line
+        {'1': 111, '4': 4, '5': 5}, combiner);
+    final map = SnapshotMap<String, int>(topMap, bottomMap, combiner);
     expect(map.isEmpty, isFalse);
     expect(map.length, equals(5));
-    expect(map.keys, equals({'1', '2', '3', '4', '5'}));
-    expect(map.values.toSet(), equals({1, 2, 3, 44, 5}));
-    expect(map['1'], equals(1));
-    expect(map['2'], equals(2));
+    expect(map.keys.toList(), equals(['1', '2', '4', '3', '5']));
+    expect(map.values.toList(), equals([123, 24, 48, 3, 5]));
+    expect(map['1'], equals(123));
+    expect(map['2'], equals(24));
     expect(map['3'], equals(3));
-    expect(map['4'], equals(44));
+    expect(map['4'], equals(48));
     expect(map['5'], equals(5));
   });
 }
